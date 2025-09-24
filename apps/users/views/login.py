adde6aaa91bbbf81c25e95f53dcff0bcd25ce606
leejiyun1 <poi20701556@gmail.com>
2025-09-24 22:47:43 +0900

    add comment

--- apps/users/views/login.py
+++ apps/users/views/login.py
@@ -10,12 +10,13 @@ class LoginView(APIView):
     """
 
     def post(self, request, *args, **kwargs):
-
+        # 요청 데이터 유효성 검사
         serializer = LoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
+        # 사용자 인증 및 토큰 생성
         user = serializer.validated_data['user']
-
         token = AuthService.generate_token_for_user(user)
 
+        # 응답 반환
         return Response({'token': token}, status=status.HTTP_200_OK)
